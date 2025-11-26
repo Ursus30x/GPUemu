2acc2f13bfe97a48d698f12c64baec8bdf6c7b4d
Ursus30x <jakubszymczyk20@gmail.com>
2025-11-26 20:15:49 +0100

    [Common][EDK2] Symlinking and applying patches in edk2

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -4,10 +4,24 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CWD="$(cd "$SCRIPT_DIR/.." && pwd)"
 cd "$CWD"
 
-# Copy gpu implementation to QEMU hardware
-cp "$CWD/gpu/gpu.c" "$CWD/qemu/hw/misc/gpu.c"
+# Symlink gpu implementation to QEMU hardware
+ln -sf "$CWD/gpu/gpu.c" "$CWD/qemu/hw/misc/gpu.c"
 
 # Apply config patches
 cd "$CWD/qemu"
 git apply "$CWD/gpu/qemu.patch"
+
+# Configure build 
+./configure --target-list="x86_64-softmmu" --enable-debug \
+ --extra-cflags="-Wno-error=redundant-decls" --enable-gtk
+
 cd "$CWD"
+
+
+# Symlink driver implementation to EDK2
+ln -sf "$CWD/OptionRom" "$CWD/edk2/OptionRom"
+
+# Apply dsc patches
+cd "$CWD/edk2"
+
+git apply "$CWD/OptionRom/OvmfPkg.patch"
