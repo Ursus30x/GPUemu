e477edb164f23a7eec6500347e08fba1e895ff02
Ursus30x <jakubszymczyk20@gmail.com>
2025-11-29 01:36:46 +0100

    [Common][EDK2] Add support for new GPU test app

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,5 +8,6 @@ cd "$CWD"
     -m 512M \
     -bios /usr/share/edk2-ovmf/x64/OVMF.4m.fd \
     -device AREK,romfile=/$CWD/edk2/Build/OptionRom.rom -monitor stdio -nodefaults -serial file:serial.log \
-    -debugcon file:debug.log -global isa-debugcon.iobase=0x402 
+    -debugcon file:debug.log -global isa-debugcon.iobase=0x402 \
+    -drive file=fat:rw:$CWD/edk2/Build/OvmfX64/DEBUG_GCC5/X64,format=raw,media=disk
    
