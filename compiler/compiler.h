f8cb75b3ff085efa71a7cafc7a4c51da652a837c
julkot1 <julian.kotlowski@gmail.com>
2026-01-04 11:45:32 +0100

    [Common] Fix cherry picks

--- compiler/compiler.h
+++ compiler/compiler.h
@@ -7,6 +7,7 @@
 #include <string.h>
 #include "vector.h"
 
+// Token Definitions
 #define TOKEN_MOVM "movm"
 #define TOKEN_MOV  "mov"
 #define TOKEN_ROTX "rotx"
@@ -120,79 +121,16 @@ typedef enum {
 } InstFormat;
 
 typedef struct {
-    const char* token;
-    uint8_t opcode;
-    uint8_t opType;
-    uint8_t argc;
+const char* token;
+uint8_t opcode;
+uint8_t opType;
+uint8_t argc;
+InstFormat format;
 } TokenDef;
 
-TokenDef token_defs[] = {
-    { TOKEN_MOVM,  INSTR_MOV,   OP_TYPE_MATRIX, 1 },
-    { TOKEN_MOV,   INSTR_MOV,   OP_TYPE_U32,    1 },
-    { TOKEN_ROTX,  INSTR_ROTX,  OP_TYPE_MATRIX, 1 },
-    { TOKEN_ROTY,  INSTR_ROTY,  OP_TYPE_MATRIX, 1 },
-    { TOKEN_IDENT, INSTR_IDENT, OP_TYPE_MATRIX, 0 },
-    { TOKEN_TRANS, INSTR_TRANS, OP_TYPE_MATRIX, 3 },
-    { TOKEN_MVP,   INSTR_MVP,   OP_TYPE_MATRIX, 0 },
-    { TOKEN_EXIT,  INSTR_EXIT,  OP_TYPE_U32,    0 },
-    { TOKEN_CMPI,  INSTR_CMP,   OP_TYPE_U32,    2 },
-    { TOKEN_CMPF,  INSTR_CMP,   OP_TYPE_F32,    2 },
-    { TOKEN_MULM,  INSTR_MUL,   OP_TYPE_MATRIX, 2 },
-    { TOKEN_MULV,  INSTR_MUL,   OP_TYPE_VEC4,   2 },
-    { TOKEN_MULI,  INSTR_MUL,   OP_TYPE_U32,    2 },
-    { TOKEN_MULF,  INSTR_MUL,   OP_TYPE_F32,    2 },
-    { TOKEN_ADDI,  INSTR_ADD,   OP_TYPE_U32,    2 },
-    { TOKEN_ADDF,  INSTR_ADD,   OP_TYPE_F32,    2 },
-    { TOKEN_SUBI,  INSTR_SUB,   OP_TYPE_U32,    2 },
-    { TOKEN_SUBF,  INSTR_SUB,   OP_TYPE_F32,    2 },
-    { TOKEN_DIVI,  INSTR_DIV,   OP_TYPE_U32,    2 },
-    { TOKEN_DIVF,  INSTR_DIV,   OP_TYPE_F32,    2 },
-    { TOKEN_MOD,   INSTR_MOD,   OP_TYPE_U32,    2 },
-    { TOKEN_COL,   INSTR_COL,   OP_TYPE_U32,    3 },
-    { TOKEN_FSAN,  INSTR_FSAN,  OP_TYPE_F32,    0 },
-    { TOKEN_BLENDI,INSTR_BLEND, OP_TYPE_U32,    3 },
-    { TOKEN_BLENDF,INSTR_BLEND, OP_TYPE_F32,    3 },
-    { TOKEN_LERPI, INSTR_LERP,  OP_TYPE_U32,    3 },
-    { TOKEN_LERPF, INSTR_LERP,  OP_TYPE_F32,    3 },
-    { TOKEN_ABSI,  INSTR_ABS,   OP_TYPE_U32,    1 },
-    { TOKEN_ABSF,  INSTR_ABS,   OP_TYPE_F32,    1 },
-    { TOKEN_SQRT,  INSTR_SQRT,  OP_TYPE_F32,    1 },
-    { TOKEN_SIN,   INSTR_SIN,   OP_TYPE_F32,    1 },
-    { TOKEN_COS,   INSTR_COS,   OP_TYPE_F32,    1 },
-    { TOKEN_CASTI, INSTR_CAST,  OP_TYPE_U32,    1 },
-    { TOKEN_CASTF, INSTR_CAST,  OP_TYPE_F32,    1 },
-    { TOKEN_LDUM,  INSTR_LDU,   OP_TYPE_MATRIX, 1 },
-    { TOKEN_LDUV,  INSTR_LDU,   OP_TYPE_VEC4,   1 },
-    { TOKEN_LDUI,  INSTR_LDU,   OP_TYPE_U32,    1 },
-    { TOKEN_LDUF,  INSTR_LDU,   OP_TYPE_F32,    1 }
-};
-
-int num_tokens = sizeof(token_defs)/sizeof(token_defs[0]);
-typedef struct  {
-    char* token;
-    uint8_t code;
-} C_FLAG_MAP;
-C_FLAG_MAP cflag_defs[] = {
-    { TOKEN_C_FLAG_EQ, C_FLAG_EQ   },     
-    { TOKEN_C_FLAG_NEQ, C_FLAG_NEQ },    
-    { TOKEN_C_FLAG_LT, C_FLAG_LT   },     
-    { TOKEN_C_FLAG_GT, C_FLAG_GT   },     
-    { TOKEN_C_FLAG_LTE, C_FLAG_LTE },    
-    { TOKEN_C_FLAG_GTE, C_FLAG_GTE }
-};
-
-
-typedef struct  {
-    uint8_t  opcode;
-    uint8_t  opType;
-    uint8_t  isCflag;
-    uint8_t  argc;
-} op_def;
-
-typedef struct
-{
-    uint8_t  type;
-    FI32 val;
+typedef struct {
+uint8_t type;
+FI32 val;
 } arg_data;
 
 typedef struct {
@@ -216,5 +154,6 @@ typedef struct {
 } Assembler;
 
 int tokenize_file(const char *filename, Vector *v);
-int process_tokens(Vector *tokens);
+void assemble(Assembler *as);
+
 #endif
