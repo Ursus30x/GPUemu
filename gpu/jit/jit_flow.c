e47adfd1695a02afb27448c5d03b39e0c31e8bee
julkot1 <julian.kotlowski@gmail.com>
2026-03-15 21:33:26 +0100

    [Qemu][Jitter] OpEntryPoint, simple UBO load, tests with input file

--- gpu/jit/jit_flow.c
+++ gpu/jit/jit_flow.c
@@ -14,30 +14,24 @@ void handle_op_function(JitContext* ctx, uint32_t res_id, uint32_t type_id, uint
     // operands[0]: Function Control 
     // operands[1]: Function Type ID 
     // TO-DO handle multiple functions and more complex signatures in the future if needed.
-    
 
     if (ctx->func == NULL) 
     {
-        LLVMTypeRef ptr_array_type = LLVMArrayType(ctx->ptr_type, MAX_BINDINGS);
-        LLVMTypeRef stride_array_type = LLVMArrayType(ctx->int_type, MAX_ATTRIBUTES);
-        LLVMTypeRef env_struct_elements[] = {
-            ptr_array_type,     
-            ptr_array_type,     
-            stride_array_type,  
-            ctx->int_type        
-        };
-        LLVMTypeRef env_struct_type = LLVMStructTypeInContext(ctx->context, env_struct_elements, 4, 0);
-
+        // Use the pre-built ExecutionContext struct type from init_jit
         // Function Signature: void main_simt(ExecutionContext* env, float* out_ptr)
         LLVMTypeRef param_types[] = { 
-            LLVMPointerType(env_struct_type, 0), 
-            LLVMPointerType(ctx->float_type, 0) 
+            LLVMPointerType(ctx->exec_ctx_type, 0), 
+            LLVMPointerType(ctx->vec_float_type, 0) 
         };
         LLVMTypeRef func_type = LLVMFunctionType(LLVMVoidTypeInContext(ctx->context), param_types, 2, 0);
         ctx->func = LLVMAddFunction(ctx->module, "main_simt", func_type);
         ctx->out_ptr_arg = LLVMGetParam(ctx->func, 1);
+        ctx->env_arg = LLVMGetParam(ctx->func, 0);
+
+        LLVMAddTargetDependentFunctionAttr(ctx->func, "no-trapping-math", "true");
+        LLVMAddTargetDependentFunctionAttr(ctx->func, "stack-protector-buffer-size", "8");
+        LLVMAddTargetDependentFunctionAttr(ctx->func, "prefer-vector-width", "256");
     }
-    
 }
 
 void handle_op_label(JitContext* ctx, uint32_t res_id) 
