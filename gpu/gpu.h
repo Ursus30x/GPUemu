9791bb6768a5ae03aeac3781509b5e6d1021c8c0
Ursus30x <jakubszymczyk20@gmail.com>
2026-04-19 22:43:33 +0200

    GPU DMA functionality

--- gpu/gpu.h
+++ gpu/gpu.h
@@ -72,13 +72,13 @@ typedef struct GpuState {
     uint32_t height;                    // 0x20
     uint32_t framebuffer_vram_offset;   // 0x24
     uint32_t gpu_time;                  // 0x28
-    uint32_t zbuffer_addr;              // 0x30
-    uint32_t int_status;                // 0x34
-    uint32_t int_mask;                  // 0x38
-    uint32_t dma_addr;                  // 0x3C
-    uint32_t dma_vram;                  // 0x40
-    uint32_t dma_size;                  // 0x44
-    uint32_t dma_cmd;                   // 0x48
+    uint32_t zbuffer_addr;              // 0x2C
+    uint32_t int_status;                // 0x30
+    uint32_t int_mask;                  // 0x34
+    uint32_t dma_addr;                  // 0x38
+    uint32_t dma_vram;                  // 0x3C
+    uint32_t dma_size;                  // 0x40
+    uint32_t dma_cmd;                   // 0x44
 
     GenericBufferConfig vbo_config;
     GenericBufferConfig edge_config;
